dba7e5e1bc5bec8e779bd02087b7f4484977b0af
yangjihong <1411251193@qq.com>
2024-07-23 19:22:30 +0800

    feat: 从多功能栏的建议触发录屏

--- prebuild/service-worker.js
+++ prebuild/service-worker.js
@@ -6,63 +6,67 @@ async function getCurrentTab() {
 }
 
 chrome.runtime.onInstalled.addListener(() => {
-  chrome.contextMenus.create({
-    id: 'startRecord',
-    title: '开始录屏',
-    contexts: ['all']
-  })
-
-  chrome.contextMenus.onClicked.addListener(async (info, tab) => {
-    if (info.menuItemId === 'startRecord') {
-      const existingContexts = await chrome.runtime.getContexts({})
-      let recording = false
-
-      const offscreenDocument = existingContexts.find(c => c.contextType === 'OFFSCREEN_DOCUMENT')
-      console.log(1111, offscreenDocument)
-      // If an offscreen document is not already open, create one.
-      if (!offscreenDocument) {
-        // Create an offscreen document.
-        await chrome.offscreen.createDocument({
-          url: 'offscreen.html',
-          reasons: ['USER_MEDIA'],
-          justification: 'Recording from chrome.tabCapture API'
-        })
-      } else {
-        recording = offscreenDocument.documentUrl.endsWith('#recording')
+  // chrome.contextMenus.create({
+  //   id: 'startRecord',
+  //   title: '开始录屏',
+  //   contexts: ['all']
+  // })
+  chrome.omnibox.onInputChanged.addListener(async (text, suggest) => {
+    suggest([
+      {
+        content: '开始录屏',
+        description: '描述',
+        deletable: false
       }
-
-      if (recording) {
-        chrome.runtime.sendMessage({
-          type: 'stop-recording',
-          target: 'offscreen'
-        })
-        chrome.action.setIcon({ path: 'icons/not-recording.png' })
-        return
-      }
-
-      const activeTab = await getCurrentTab()
-      console.log('activeTab', activeTab)
-
-      // Get a MediaStream for the active tab.
-      const streamId = await chrome.tabCapture.getMediaStreamId({
-        targetTabId: activeTab.id
-      })
-
-      // Send the stream ID to the offscreen document to start recording.
-      chrome.runtime.sendMessage({
-        type: 'start-recording',
-        target: 'offscreen',
-        data: streamId
-      })
-
-      chrome.action.setIcon({ path: '/icons/recording.png' })
-    }
+    ])
   })
-
-  chrome.action.onClicked.addListener(async tab => {
-    console.log('点击icon', tab)
-    chrome.sidePanel.setPanelBehavior({
-      openPanelOnActionClick: true
-    })
+  chrome.omnibox.onInputEntered.addListener(async () => {
+    console.log(333)
+    await startRecord()
   })
 })
+
+const startRecord = async () => {
+  const existingContexts = await chrome.runtime.getContexts({})
+  let recording = false
+
+  const offscreenDocument = existingContexts.find(c => c.contextType === 'OFFSCREEN_DOCUMENT')
+  console.log(1111, offscreenDocument)
+  // If an offscreen document is not already open, create one.
+  if (!offscreenDocument) {
+    // Create an offscreen document.
+    await chrome.offscreen.createDocument({
+      url: 'offscreen.html',
+      reasons: ['USER_MEDIA'],
+      justification: 'Recording from chrome.tabCapture API'
+    })
+  } else {
+    recording = offscreenDocument.documentUrl.endsWith('#recording')
+  }
+
+  if (recording) {
+    chrome.runtime.sendMessage({
+      type: 'stop-recording',
+      target: 'offscreen'
+    })
+    chrome.action.setIcon({ path: 'icons/not-recording.png' })
+    return
+  }
+
+  const activeTab = await getCurrentTab()
+  console.log('activeTab', activeTab)
+
+  // Get a MediaStream for the active tab.
+  const streamId = await chrome.tabCapture.getMediaStreamId({
+    targetTabId: activeTab.id
+  })
+
+  // Send the stream ID to the offscreen document to start recording.
+  chrome.runtime.sendMessage({
+    type: 'start-recording',
+    target: 'offscreen',
+    data: streamId
+  })
+
+  chrome.action.setIcon({ path: '/icons/recording.png' })
+}
